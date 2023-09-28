628b6d957ec5ea2e15fb2bacdf04e7aab70f780a
unknown <ilhamsetyadji28@gmail.com>
2023-09-28 13:10:56 +0700

    done

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
         className="font-titleFont text-lg text-textGreen font-semibold flex
       items-center tracking-wide"
       >
-        04. What's Next?
+        04. Whats Next?
       </p>
       <h2 className="font-titleFont text-5xl font-semibold">Get In Touch</h2>
       <p className="max-w-[600px] text-center text-textDark">
